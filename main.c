83fb030eb135b1469c88a09f767172a51ab2011f
kgh980316 <kgh980316@gmail.com>
2021-10-22 21:02:40 +0900

    debugging

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 	printf("Hello master\n");
-	printf("I'm added branch1!\n);
+	printf("I'm added branch1!\n");
 	printf("I'm branch2\n");
 	printf("I'm branch3\n");
 	return 0;
